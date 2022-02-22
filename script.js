df2c5532e5a1013eea95804eb3a1c803eff31fed
Spnarkdnark <samuelfudala@gmail.com>
2022-02-21 19:39:09 -0500

    done

---
+++ script.js
@@ -0,0 +1,15 @@
+const photoSwap = function(){
+    let randomInt = getRandom(31);
+    photo.src = `photos/${randomInt}.jpeg`;
+    console.log(randomInt);
+}
+
+const getRandom = function(range){
+    return Math.floor(Math.random() * range);
+}
+
+const photo = document.querySelector('#sam');
+const button = document.querySelector('.button');
+button.addEventListener('click', photoSwap)
+
+
